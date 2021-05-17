a5d5f386bc9d6a5d31886dfcc008da7265237b87
cherrytora <45777647+cherrytora@users.noreply.github.com>
2021-05-18 07:07:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,32 +8,9 @@ import io
 
 # initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
-#model = None
-
-# def load_model():
-# 	# load the pre-trained Keras model (here we are using a model
-# 	# pre-trained on ImageNet and provided by Keras, but you can
-# 	# substitute in your own networks just as easily)
-# 	global model
-# 	model = ResNet50(weights="imagenet")
-
 model = ResNet50(weights="imagenet")
 
-# def prepare_image(image, target):
-# 	# if the image mode is not RGB, convert it
-# 	if image.mode != "RGB":
-# 		image = image.convert("RGB")
 
-# 	# resize the input image and preprocess it
-# 	image = image.resize(target)
-# 	image = img_to_array(image)
-# 	image = np.expand_dims(image, axis=0)
-# 	image = imagenet_utils.preprocess_input(image)
-
-# 	# return the processed image
-# 	return image
-
-# @app.route('/', methods=["POST"])
 @app.route("/predict", methods=["POST"])
 def predict():
 	# initialize the data dictionary that will be returned from the
@@ -50,7 +27,6 @@ def predict():
 				image = image.convert("RGB")
 
 			# preprocess the image and prepare it for classification
-			#image = prepare_image(image, target=(224, 224))
 			image = image.resize((224, 224))
 			image = img_to_array(image)
 			image = np.expand_dims(image, axis=0)
